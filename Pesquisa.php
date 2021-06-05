7ed30e93f8e2fd0628b5d4e3926114b464217717
rafael135 <rtrafaeltrova@gmail.com>
2021-06-04 22:23:08 -0300

    Atualização página de pesquisa
    Página de pesquisa já está com os principais filtros funcionais

--- Pesquisa.php
+++ Pesquisa.php
@@ -1,33 +1,34 @@
 <?php
 
-    require __DIR__.'/vendor/autoload.php';
-    require_once __DIR__ . '/vendor/mobiledetect/mobiledetectlib/Mobile_Detect.php';
-    session_start();
-    $detect = new Mobile_Detect();
+require __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/vendor/mobiledetect/mobiledetectlib/Mobile_Detect.php';
+session_start();
+$detect = new Mobile_Detect();
 
 
-    if($detect->isMobile() == true && !$detect->isTablet()){
-        //  Inclui a navbar mobile na página
-        include __DIR__ . '/includesPag/navbarSideBar.php';
-    }else{
-        // Inclui a navbar para Desktop na página
-        include __DIR__.'/includesPag/navbar.php';
-    }
+if ($detect->isMobile() == true && !$detect->isTablet()) {
+    //  Inclui a navbar mobile na página
+    include __DIR__ . '/includesPag/navbarSideBar.php';
+} else {
+    // Inclui a navbar para Desktop na página
+    include __DIR__ . '/includesPag/navbar.php';
+}
 
-    ?>
+?>
 
-    <head>
-        <link rel="stylesheet" href="includes/pesquisa.css">
-    </head>
+<head>
+    <link rel="stylesheet" href="includes/pesquisa.css">
+    <link rel="stylesheet" href="includes/publicacao.css">
+</head>
 
-    <?php
+<?php
 
-    // Inclui o formulário de pesquisa
-    //include __DIR__ . '/includesPag/pesquisa.php';
+// Inclui o formulário de pesquisa
+//include __DIR__ . '/includesPag/pesquisa.php';
 
-    // Inclui a página Sobre Nós
-    include __DIR__ . '/includesPag/PesquisaResultado.php';
+// Inclui a página Sobre Nós
+include __DIR__ . '/includesPag/PesquisaResultado.php';
 
-    //  Inclui o final da página
-    include __DIR__.'/includesPag/footer.php';
+//  Inclui o final da página
+include __DIR__ . '/includesPag/footer.php';
 ?>
--- includesPag/PesquisaResultado.php
+++ includesPag/PesquisaResultado.php
@@ -5,10 +5,10 @@
 
     <?php
 
-use App\money_format;
-use App\Produto;
+    use App\money_format;
+    use App\Produto;
 
-if ($_GET) {
+    if ($_GET) {
         $txt = $_GET['txtPesquisa'];
         $frete = $_GET['frete'];
         if ($frete == "1") {
@@ -35,6 +35,39 @@ if ($_GET) {
     }
     ?>
 
+    <script src="JQuery 3.6.0/jquery-3.6.0.min.js"></script>
+    <script src="JQuery 3.6.0/jquery.zoom.min.js"></script>
+    <script src="ActionsJS/imgZoom.js"></script>
+
+    <script>
+        jQuery(document).ready(function() {
+            /*  Replace all SVG images with inline SVG */
+            $('img.svg').each(function() {
+                var $img = $(this);
+                var imgID = $img.attr('id');
+                var imgClass = $img.attr('class');
+                var imgURL = $img.attr('src');
+
+                $.get(imgURL, function(data) {
+                    // Get the SVG tag, ignore the rest
+                    var $svg = $(data).find('svg');
+                    // Add replaced image's ID to the new SVG
+                    if (typeof imgID !== 'undefined') {
+                        $svg = $svg.attr('id', imgID);
+                    }
+                    // Add replaced image's classes to the new SVG
+                    if (typeof imgClass !== 'undefined') {
+                        $svg = $svg.attr('class', imgClass + ' replaced-svg');
+                    }
+                    // Remove any invalid XML tags as per http://validator.w3.org
+                    $svg = $svg.removeAttr('xmlns:a');
+                    // Replace image with new SVG
+                    $img.replaceWith($svg);
+                });
+            });
+        });
+    </script>
+
     <!--Coluna 1-->
     <div class="container-fluid m-0 p-0 mx-2 ms-0 text-center pt-0">
         <div class="row">
@@ -46,75 +79,116 @@ if ($_GET) {
                             <input type="text" class="form-control rounded-2" id="txtPesquisa" name="txtPesquisa" placeholder="Pesquisa" value="<?php echo ($txt); ?>">
                             <label for="txtPesquisa">Pesquisa</label>
                         </div>
-                        <div class="container-fluid border-produto p-0 m-0 p-2">
-                            <div class="form-check form-switch mb-3">
-                                <input class="form-check-input" type="radio" name="filtro" id="flexSwitchCheckDefault">
-                                <label class="form-check-label" for="flexSwitchCheckDefault">Eletrodomésticos</label>
-                            </div>
 
-                            <div class="form-check form-switch mb-3">
-                                <input class="form-check-input" type="radio" name="filtro" id="flexSwitchCheckDefault">
-                                <label class="form-check-label" for="flexSwitchCheckDefault">Áudio</label>
-                            </div>
+                        <div class="row justify-content-center align-items-center pt-3">
+                            <div class="col-12">
+                                <button class="btn btn-pub rounded-0 w-100 border border-1" style="max-height:50px;" type="button" data-bs-toggle="collapse" data-bs-target="#collapseDescricao" aria-expanded="false" aria-controls="collapseDescricao">
+                                    <div class="row">
+                                        <div class="col-10">
+                                            <div class="container-fluid my-auto p-0">
+                                                <p class="text-start align-self-center m-auto fs-5 h-100 w-100">Departamentos</p>
+                                            </div>
+                                        </div>
 
-                            <div class="form-check form-switch mb-3">
-                                <input class="form-check-input" type="radio" name="filtro" id="flexSwitchCheckDefault">
-                                <label class="form-check-label" for="flexSwitchCheckDefault">Brinquedos</label>
-                            </div>
-
-                            <div class="form-check form-switch mb-3">
-                                <input class="form-check-input" type="radio" name="filtro" id="flexSwitchCheckDefault">
-                                <label class="form-check-label" for="flexSwitchCheckDefault">Móveis</label>
-                            </div>
-
-                            <div class="form-check form-switch mb-3">
-                                <input class="form-check-input" type="radio" name="filtro" id="flexSwitchCheckDefault">
-                                <label class="form-check-label" for="flexSwitchCheckDefault">Serviços</label>
-                            </div>
-
-                            <div class="form-check form-switch mb-3">
-                                <input class="form-check-input" type="radio" name="filtro" id="flexSwitchCheckDefault">
-                                <label class="form-check-label" for="flexSwitchCheckDefault">Games</label>
-                            </div>
-
-                            <div class="form-check form-switch mb-3">
-                                <input class="form-check-input" type="radio" name="filtro" id="flexSwitchCheckDefault">
-                                <label class="form-check-label" for="flexSwitchCheckDefault">Informática</label>
-                            </div>
-
-                            <div class="form-check form-switch mb-3">
-                                <input class="form-check-input" type="radio" name="filtro" id="flexSwitchCheckDefault">
-                                <label class="form-check-label" for="flexSwitchCheckDefault">Esporte</label>
-                            </div>
-
-                            <div class="form-check form-switch mb-3">
-                                <input class="form-check-input" type="radio" name="filtro" id="flexSwitchCheckDefault">
-                                <label class="form-check-label" for="flexSwitchCheckDefault">Livros</label>
-                            </div>
-
-                            <div class="form-check form-switch mb-3">
-                                <input class="form-check-input" type="radio" name="filtro" id="flexSwitchCheckDefault">
-                                <label class="form-check-label" for="flexSwitchCheckDefault">Roupas</label>
-                            </div>
-
-                            <div class="form-check form-switch mb-3">
-                                <input class="form-check-input" type="radio" name="filtro" id="flexSwitchCheckDefault">
-                                <label class="form-check-label" for="flexSwitchCheckDefault">Acessórios</label>
-                            </div>
-
-                            <div class="form-check form-switch mb-3">
-                                <input class="form-check-input" type="radio" name="filtro" id="flexSwitchCheckDefault">
-                                <label class="form-check-label" for="flexSwitchCheckDefault">Alimentos</label>
+                                        <div class="expand col-2 d-flex justify-content-end">
+                                            <img class="svg expand-more svg-btn" src="img/btns/expand.svg"></img>
+                                        </div>
+                                    </div>
+                                </button>
                             </div>
                         </div>
 
+                        <div class="collapse mt-0 mb-0" id="collapseDescricao">
+                            <div class="container-fluid container-search text-blue p-2 m-0">
+                                <div class="form-check form-switch mb-3">
+                                    <input class="form-check-input" type="radio" name="filtro" id="flexSwitchCheckDefault">
+                                    <label class="form-check-label" for="flexSwitchCheckDefault">Eletrodomésticos</label>
+                                </div>
+
+                                <div class="form-check form-switch mb-3">
+                                    <input class="form-check-input" type="radio" name="filtro" id="flexSwitchCheckDefault">
+                                    <label class="form-check-label" for="flexSwitchCheckDefault">Áudio</label>
+                                </div>
+
+                                <div class="form-check form-switch mb-3">
+                                    <input class="form-check-input" type="radio" name="filtro" id="flexSwitchCheckDefault">
+                                    <label class="form-check-label" for="flexSwitchCheckDefault">Brinquedos</label>
+                                </div>
+
+                                <div class="form-check form-switch mb-3">
+                                    <input class="form-check-input" type="radio" name="filtro" id="flexSwitchCheckDefault">
+                                    <label class="form-check-label" for="flexSwitchCheckDefault">Móveis</label>
+                                </div>
+
+                                <div class="form-check form-switch mb-3">
+                                    <input class="form-check-input" type="radio" name="filtro" id="flexSwitchCheckDefault">
+                                    <label class="form-check-label" for="flexSwitchCheckDefault">Serviços</label>
+                                </div>
+
+                                <div class="form-check form-switch mb-3">
+                                    <input class="form-check-input" type="radio" name="filtro" id="flexSwitchCheckDefault">
+                                    <label class="form-check-label" for="flexSwitchCheckDefault">Games</label>
+                                </div>
+
+                                <div class="form-check form-switch mb-3">
+                                    <input class="form-check-input" type="radio" name="filtro" id="flexSwitchCheckDefault">
+                                    <label class="form-check-label" for="flexSwitchCheckDefault">Informática</label>
+                                </div>
+
+                                <div class="form-check form-switch mb-3">
+                                    <input class="form-check-input" type="radio" name="filtro" id="flexSwitchCheckDefault">
+                                    <label class="form-check-label" for="flexSwitchCheckDefault">Esporte</label>
+                                </div>
+
+                                <div class="form-check form-switch mb-3">
+                                    <input class="form-check-input" type="radio" name="filtro" id="flexSwitchCheckDefault">
+                                    <label class="form-check-label" for="flexSwitchCheckDefault">Livros</label>
+                                </div>
+
+                                <div class="form-check form-switch mb-3">
+                                    <input class="form-check-input" type="radio" name="filtro" id="flexSwitchCheckDefault">
+                                    <label class="form-check-label" for="flexSwitchCheckDefault">Roupas</label>
+                                </div>
+
+                                <div class="form-check form-switch mb-3">
+                                    <input class="form-check-input" type="radio" name="filtro" id="flexSwitchCheckDefault">
+                                    <label class="form-check-label" for="flexSwitchCheckDefault">Acessórios</label>
+                                </div>
+
+                                <div class="form-check form-switch mb-3">
+                                    <input class="form-check-input" type="radio" name="filtro" id="flexSwitchCheckDefault">
+                                    <label class="form-check-label" for="flexSwitchCheckDefault">Alimentos</label>
+                                </div>
+                            </div>
+
+                        </div>
+
+
+
+                        <hr class="mx-1" />
                         <div class="container-fluid p-0 px-3 m-0 justify-content-center align-items-center">
-                            <hr class="mx-1" />
+
                             <div class="form-check form-switch">
                                 <input class="form-check-input" <?php if ($frete == 1) {
                                                                     echo ("checked");
                                                                 } ?> type="checkbox" id="checkFrete" name="checkFrete">
                                 <label class="form-check-label text-blue  fw-bold" for="checkFrete">Frete grátis</label>
+                            </div>
+                        </div>
+
+                        <hr class="mx-1">
+
+                        <div class="container-fluid p-0 m-0">
+                            <div class="row gx-0">
+                                <div class="col-6">
+                                    <input type="radio" class="btn-check w-100 me-0" name="optPublicacao" id="optProduto" style="border-bottom-right-radius: 0 !important; border-top-right-radius: 0 !important;" autocomplete="off" checked>
+                                    <label class="btn btn-outline-light btn-radio-blue w-100 me-0" for="optProduto" style="border-bottom-right-radius: 0 !important; border-top-right-radius: 0 !important;">Produto</label>
+                                </div>
+
+                                <div class="col-6">
+                                    <input type="radio" class="btn-check w-100 ms-0" name="optPublicacao" id="optServico" style="border-bottom-left-radius: 0 !important; border-top-left-radius: 0 !important;" autocomplete="off">
+                                    <label class="btn btn-outline-light btn-radio-blue w-100 ms-0" for="optServico" style="border-bottom-left-radius: 0 !important; border-top-left-radius: 0 !important;">Serviço</label>
+                                </div>
                             </div>
                         </div>
 
@@ -139,11 +213,21 @@ if ($_GET) {
                         <div class="container-fluid p-0 px-3 m-0">
                             <div class="form-floating rounded-0">
                                 <select class="form-select border border-1 rounded-0" id="selectFiltrar" name="selectFiltrar" aria-label="Filtrar por">
-                                    <option value="1" <?php if($filtro == 1){ echo("selected"); } ?>>Preço decrescente</option>
-                                    <option value="2" <?php if($filtro == 2){ echo("selected"); } ?>>Preço crescente</option>
-                                    <option value="3" <?php if($filtro == 3){ echo("selected"); } ?>>Melhor avaliados</option>
-                                    <option value="4" <?php if($filtro == 4){ echo("selected"); } ?>>Mais novos</option>
-                                    <option value="5" <?php if($filtro == 5){ echo("selected"); } ?>>Mais velhos</option>
+                                    <option value="1" <?php if ($filtro == 1) {
+                                                            echo ("selected");
+                                                        } ?>>Preço decrescente</option>
+                                    <option value="2" <?php if ($filtro == 2) {
+                                                            echo ("selected");
+                                                        } ?>>Preço crescente</option>
+                                    <option value="3" <?php if ($filtro == 3) {
+                                                            echo ("selected");
+                                                        } ?>>Melhor avaliados</option>
+                                    <option value="4" <?php if ($filtro == 4) {
+                                                            echo ("selected");
+                                                        } ?>>Mais novos</option>
+                                    <option value="5" <?php if ($filtro == 5) {
+                                                            echo ("selected");
+                                                        } ?>>Mais velhos</option>
                                 </select>
                                 <label class="form-label" for="selectFiltrar">Filtrar por</label>
                             </div>
@@ -157,7 +241,7 @@ if ($_GET) {
             <div class="col-lg-9 pt-3">
                 <div class="row mx-auto">
 
-                    <?php foreach($resultado as $produto) {
+                    <?php foreach ($resultado as $produto) {
                         $idProduto = $produto['idProduto'];
                         $idAutor = $produto['idAutor'];
                         $titulo = $produto['titulo'];
@@ -172,12 +256,12 @@ if ($_GET) {
                         }
                     ?>
                         <div class="col-sm-4 col-md-3 col-lg-2">
-                            <a href="produto.php?id=<?php echo($idProduto) ?>">
+                            <a href="produto.php?id=<?php echo ($idProduto) ?>">
                                 <div class="card mb-4" style="width: 12rem; height: 12rem;">
-                                    <img src="<?php echo($destinoImg); ?>" class="card-img-top" alt="...">
+                                    <img src="<?php echo ($destinoImg); ?>" class="card-img-top" alt="...">
                                     <div class="card-body">
-                                        <h5 class="card-title"><?php echo($titulo) ?></h5>
-                                        <p class="card-text"><?php echo($money->money_format("%.2n", $preco)); ?></p>
+                                        <h5 class="card-title"><?php echo ($titulo) ?></h5>
+                                        <p class="card-text"><?php echo ($money->money_format("%.2n", $preco)); ?></p>
 
                                     </div>
                                 </div>
