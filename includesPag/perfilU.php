0931d71720890145b92cdb64ec9f2da2146027da
rafael135 <rtrafaeltrova@gmail.com>
2021-06-04 21:55:56 -0300

    correção

--- includesPag/perfilU.php
+++ includesPag/perfilU.php
@@ -276,15 +276,16 @@ if ($_GET) {
                                     ?>
 
                                                     <div class="col-sm-6 col-md-4 col-lg-2">
-                                                        <a class="text-white" href="produto.php?id=<?php echo ($idProduto); ?>">
+                                                        <!--<a href="produto.php?id=<?php echo ($idProduto); ?>" title="Editar publicação">-->
+                                                        <a href="editarPub.php?id=<?php echo ($idProduto); ?>" title="Editar publicação">
                                                             <div class="card link text-white text-center mb-2 mx-0" style="max-height: 12rem !important; max-width: 12rem !important;">
                                                                 <img class="card-img" src="<?php echo ($destinoImg); ?>" alt="">
-                                                                <div class="card-img-overlay p-0 m-0">
+                                                                <!--<div class="card-img-overlay py-0">
                                                                     <div class="row px-lg-3">
                                                                         <div class="col-4 offset-8">
                                                                             <div class="row">
                                                                                 <div class="col-6">
-                                                                                    <a class="btn btn-clean btn-edit-pub m-0 p-0 d-block text-white" href="editarPub.php?id=<?php echo ($idProduto); ?>"><span class="material-icons m-0 p-0 text-danger">edit</span></a>
+                                                                                    <a class="btn btn-clean btn-edit-pub m-0 p-0 text-white" href="editarPub.php?id=<?php echo ($idProduto); ?>"><span class="material-icons m-0 p-0 text-light">edit</span></a>
                                                                                 </div>
                                                                                 <div class="col-6">
                                                                                     <a class="btn btn-clean btn-delete-pub m-0 p-0 text-white" href="ActionPHP/excluirPub.php?id=<?php echo ($idProduto); ?>"><span class="material-icons m-0 p-0 red">delete</span></a>
@@ -292,7 +293,7 @@ if ($_GET) {
                                                                             </div>
                                                                         </div>
                                                                     </div>
-                                                                </div>
+                                                                </div>-->
                                                                 <div class="card-body">
                                                                     <h4 cklass="card-title text-white"><?php echo ($titulo); ?></h4>
                                                                     <?php setlocale(LC_MONETARY, 'pt_BR.UTF8');
@@ -321,7 +322,7 @@ if ($_GET) {
                 </div>
 
             </div>
-            <?php  ?>
+
         </div>
 
     </div>
