f3235af40fb5fbbbf38bec5fe0100c0352521244
Isaque900 <78832965+Isaque900@users.noreply.github.com>
2021-06-23 22:34:00 -0300

    Tela das informações da venda (o cara disse q era pra ser simples kkk))

---
+++ includesPag/TelaInfoVenda.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Informações do comprador</title>
+
+    <link rel="stylesheet" href="bootstrap-5.0.0B3/css/bootstrap.min.css">
+
+</head>
+
+<body>
+
+    <div class="container text-center my-5">
+        <div class="card text-dark bg-light">
+            <div class="card-header">
+                <h1>Informações da venda</h1>
+            </div>
+
+            <h2 class="mt-4 py-3" id="NomeComprador">Nome: Bolsolula</h2>
+
+            <h2 class="py-3" id="EnderComprador">Endereço: Brasília, Rua do Lázaro 24</h2>
+
+            <h2 class="py-3" id="MetodoPagamento">Método de pagamento: Bitcoin</h2>
+
+            <h2 class="py-3" id="QuantPaga">Quantia paga: R$350,00</h2>
+
+        </div>
+    </div>
+
+    <script src="bootstrap-5.0.0B3/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
